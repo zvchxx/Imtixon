d12136fd253d88315099a8e545250501a90d111f
zvchx <zzvchx@outlook.com>
2024-08-13 17:06:45 +0500

    Add super admin menu

--- Madad Ta'lim/main.py
+++ Madad Ta'lim/main.py
@@ -27,7 +27,29 @@ def show_auth_menu():
     except KeyboardInterrupt:
         return show_auth_menu()
     
+
+def super_admin_menu():
+    text = """
+    1. Show all users
+    2. Show all posts
+    3. Search with keywords   
+    4. Quit
+"""
+    print(text)
+    try:
+        user_input = input("Enter your choice: ")
+        if user_input == "1":
+            pass
+        elif user_input == "4":
+            print("\nThakns for wisit")
+            super_admin_menu()
+        else:
+            print("\nWrong choice !")
+            return super_admin_menu()()
+    except KeyboardInterrupt:
+        return super_admin_menu()()
     
+
 def admin_menu():
     text = """
     1. Show all users
